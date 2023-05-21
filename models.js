4329176f4027043c0de871eeafd76e9d9a5ef5c9
CodeCriminal365 <120529066+CodeCriminal365@users.noreply.github.com>
2023-05-21 10:05:56 -0700

    Delete models.js

--- models.js
+++
@@ -1,7 +0,0 @@
-function Models() {
-        return <a href="https://github.com/ChatDudeAI/ChatDudeAI">ChatDude - Our flagship model.</a><br>;
-      }
-
-const container = document.getElementById('models');
-const root = ReactDOM.createRoot(container);
-root.render(<Models />)
