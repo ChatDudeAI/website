006d9f29f4427b9a1e188ecdcad00fe48403c45a
CodeCriminal365 <120529066+CodeCriminal365@users.noreply.github.com>
2023-05-21 10:01:13 -0700

    Create models.js

---
+++ models.js
@@ -0,0 +1,7 @@
+function Models() {
+        return <h1>Hello World!</h1>;
+      }
+
+const container = document.getElementById('models');
+const root = ReactDOM.createRoot(container);
+root.render(<Models />)
